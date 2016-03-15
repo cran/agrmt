4bbcdb155571ea0d1d599ed1a63be887ca0c1f88
Didier Ruedin <didier.ruedin@wolfson.oxon.org>
2016-03-15 10:52:51 +0000

    version 1.40.1

--- R/MRQ.R
+++ R/MRQ.R
@@ -1,5 +1,7 @@
 MRQ <- function(Z) {
   # polarization index (Montalvo and Reynal-Querol 2005); cited in Ziller 2014
   z <- length(Z)
+  # standardizing frequency vector if necessary
+  if (!sum(V) == 1) V <- V/sum(V)
   4 * sum(Z[1:z]^2*(1-Z))
   }
